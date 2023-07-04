82e9ac20275278f77c7b3933e213eacf9fbb3d99
1cecreamm <teyengkuroba@gmail.com>
2023-07-04 11:38:20 +0700

    Initial

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_munch.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_munch-user \
+    derp_munch-userdebug \
+    derp_munch-eng
---
+++ derp_munch.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+PRODUCT_NAME := derp_munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Tag official
+DERP_BUILDTYPE := Official
+
+# Inherit some common device props
+TARGET_SUPPORTS_QUICK_TAP := true
+
+BUILD_FINGERPRINT := Redmi/munch/munch:12/RKQ1.211001.001/V13.0.5.0.SLMMIXM:user/release-keys
